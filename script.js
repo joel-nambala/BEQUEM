fd43b2426055df54b6dab94b41acfd817aab5744
PROF. JOEL <joel.n.wanjala@gmail.com>
2023-05-11 15:18:23 +0300

    Added the about us page

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 // Select DOM elements
 const copyYear = document.querySelector('.copy-year');
+const btnModal = document.querySelectorAll('.btn-modal');
+const contactModal = document.querySelector('.contact');
+const contactFormModal = document.querySelector('.contact-form');
 
-// Functions
+// Change copyright year
 const changeCopyrightYear = function () {
   // Get the present year
   const today = new Date().getFullYear();
@@ -12,3 +15,22 @@ const changeCopyrightYear = function () {
   copyYear.textContent = today;
 };
 changeCopyrightYear();
+
+// Show modal
+const showModal = function (e) {
+  e.preventDefault();
+  contactModal.classList.add('show-modal');
+  contactFormModal.classList.add('show-modal');
+};
+
+// Hide modal
+const hideModal = function () {
+  contactModal.classList.remove('show-modal');
+  contactFormModal.classList.remove('show-modal');
+};
+
+btnModal.forEach(function (btn, i, arr) {
+  btn.addEventListener('click', showModal);
+});
+
+contactModal.addEventListener('click', hideModal);
