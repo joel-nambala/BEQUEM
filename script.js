48911291b7314759d0ffcd7cefc981dc6f03e721
PROF. JOEL <joel.n.wanjala@gmail.com>
2023-05-12 16:18:27 +0300

    Added the package.json file and the navigation bar responsiveness

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const copyYear = document.querySelector('.copy-year');
 const btnModal = document.querySelectorAll('.btn-modal');
 const contactModal = document.querySelector('.contact');
 const contactFormModal = document.querySelector('.contact-form');
+const navRespond = document.querySelector('.nav-respond');
+const linksContainer = document.querySelector('.links-container');
+const navList = document.querySelector('.nav-list');
+const nav = document.querySelector('.nav');
 
 // Change copyright year
 const changeCopyrightYear = function () {
@@ -34,3 +38,29 @@ btnModal.forEach(function (btn, i, arr) {
 });
 
 contactModal.addEventListener('click', hideModal);
+
+// Responsive navigation bar
+const responsiveNavigation = function () {
+  // Calculate the heights
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const navHeight = navList.getBoundingClientRect().height;
+
+  // Append the heights
+  if (containerHeight === 0) linksContainer.style.height = `${navHeight}px`;
+  else linksContainer.style.height = 0;
+};
+
+navRespond.addEventListener('click', responsiveNavigation);
+
+// Fixed navigation bar
+const fixedNavigation = function () {
+  // Calculate the heights
+  const scrollHeight = window.scrollY;
+  const navHeight = navList.getBoundingClientRect().height;
+
+  // Append the navigation
+  if (scrollHeight > navHeight) nav.classList.add('fixed');
+  else nav.classList.remove('fixed');
+};
+
+window.addEventListener('scroll', fixedNavigation);
