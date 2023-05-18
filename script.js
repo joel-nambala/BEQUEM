f447cba018048ff4f2a75460065392cb2a45dee5
PROF. JOEL <joel.n.wanjala@gmail.com>
2023-05-18 15:44:24 +0300

    Changed the smooth scrolling event and added responsiveness

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const navRespond = document.querySelector('.nav-respond');
 const linksContainer = document.querySelector('.links-container');
 const navList = document.querySelector('.nav-list');
 const nav = document.querySelector('.nav');
+const scrollLink = document.querySelectorAll('.scroll-link');
 
 // Change copyright year
 const changeCopyrightYear = function () {
@@ -64,3 +65,38 @@ const fixedNavigation = function () {
 };
 
 window.addEventListener('scroll', fixedNavigation);
+
+// Smooth scroll
+scrollLink.forEach(function (link, i, arr) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    // Get the href attribute
+    const id = link.getAttribute('href');
+
+    // Select the section to scroll to
+    const element = document.querySelector(id);
+    if (element === null) return;
+
+    // Calculate the heights
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const navHeight = nav.getBoundingClientRect().height;
+    const fixedNav = nav.classList.contains('fixed');
+
+    // Get the position of the element
+    let position = element.offsetTop - navHeight;
+
+    // Change the offset position of an element
+    if (!fixedNav) position = position - navHeight;
+    if (navHeight > 82) position = position + containerHeight;
+
+    // Scroll behaviour
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: 'smooth',
+    });
+
+    // Set the links container height back to zero
+    linksContainer.style.height = 0;
+  });
+});
